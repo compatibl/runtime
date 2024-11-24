a7f89c79adb638765f3cf73b5260720785872849
Alexander Sokol <sokol@compatibl.com>
2024-11-24 12:46:59 -0500

    Add tests to test_basic_mongo_db.
    (cherry picked from commit 94944ba8ae2855a63165af780e8f1934c0bffb43)
    (cherry picked from commit 9f39ab53641eb8f71f61ad78dd68b558645e5389)

--- tests/cl/runtime/db/mongo/test_basic_mongo_db.py
+++ tests/cl/runtime/db/mongo/test_basic_mongo_db.py
@@ -12,15 +12,240 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import time
+from typing import Iterable, Any
+
 import pytest
+
 from cl.runtime.context.testing_context import TestingContext
 from cl.runtime.db.mongo.basic_mongo_db import BasicMongoDb
 from cl.runtime.records.class_info import ClassInfo
-from stubs.cl.runtime import StubDataclassDerivedRecord
+from stubs.cl.runtime import StubDataclassDerivedRecord, StubDataclassNestedFields, StubDataclassComposite, \
+    StubDataclassDerivedFromDerivedRecord, StubDataclassOtherDerivedRecord, StubDataclassListFields, \
+    StubDataclassOptionalFields, StubDataclassDictFields, StubDataclassDictListFields, StubDataclassListDictFields, \
+    StubDataclassPrimitiveFields, StubDataclassSingleton
 from stubs.cl.runtime.records.for_dataclasses.stub_dataclass_record import StubDataclassRecord
 
 
+def _assert_equals_iterable_without_ordering(iterable: Iterable[Any], other_iterable: Iterable[Any]) -> bool:
+    """Checks that two iterables contain the same elements, regardless of order."""
+
+    iterable_as_list = list(iterable) if not isinstance(iterable, list) else iterable
+    other_iterable_as_list = list(other_iterable) if not isinstance(other_iterable, list) else other_iterable
+
+    if len(iterable_as_list) != len(other_iterable_as_list):
+        raise ValueError(f"Iterables have different length: {len(iterable_as_list)} and {len(other_iterable_as_list)}")
+
+    for item in iterable_as_list:
+        if item not in other_iterable_as_list:
+            raise ValueError(f"Item {item} contains only in first iterable.")
+
+    return True
+
+
 @pytest.mark.skip("Requires MongoDB server.")  # TODO: Switch test to MongoMock
+def test_smoke():
+    """Smoke test."""
+
+    # TODO: Do not hardcode DB name
+    db_class = ClassInfo.get_class_path(BasicMongoDb)
+    with TestingContext(db_class=db_class) as context:
+        # Create test record and populate with sample data
+        record = StubDataclassRecord()
+        key = record.get_key()
+
+        # Save a single record
+        context.save_many([record])
+
+        # Load using record or key
+        loaded_records = context.load_many(StubDataclassRecord, [record, key, None])
+        assert loaded_records[0] is record  # Same object is returned without lookup
+        assert loaded_records[1] == record  # Not the same object but equal
+        assert loaded_records[2] is None
+
+        assert context.load_one(StubDataclassRecord, record) is record  # Same object is returned without lookup
+        assert context.load_one(StubDataclassRecord, key) == record  # Not the same object but equal
+
+
+def test_complex_records():
+    """Test 'save_many' method for various types."""
+
+    db_class = ClassInfo.get_class_path(BasicMongoDb)
+    with TestingContext(db_class=db_class) as context:
+        samples = [
+            StubDataclassRecord(id="abc1"),
+            StubDataclassNestedFields(id="abc2"),
+            StubDataclassComposite(),
+            StubDataclassDerivedRecord(id="abc3"),
+            StubDataclassDerivedFromDerivedRecord(id="abc4"),
+            StubDataclassOtherDerivedRecord(id="abc5"),
+            StubDataclassListFields(id="abc6"),
+            StubDataclassOptionalFields(id="abc7"),
+            StubDataclassDictFields(id="abc8"),
+            StubDataclassDictListFields(id="abc9"),
+            StubDataclassListDictFields(id="abc10"),
+            StubDataclassPrimitiveFields(key_str_field="abc11"),
+            StubDataclassSingleton(),
+        ]
+
+        context.save_many(samples)
+
+        sample_keys = [sample.get_key() for sample in samples]
+        loaded_records = [context.load_one(type(key), key) for key in sample_keys]
+
+        assert loaded_records == samples
+
+
+def test_basic_operations():
+    """Test save/load/delete methods for various types."""
+
+    db_class = ClassInfo.get_class_path(BasicMongoDb)
+    with TestingContext(db_class=db_class) as context:
+        samples = [
+            StubDataclassRecord(id="abc1"),
+            StubDataclassNestedFields(id="abc2"),
+            StubDataclassComposite(),
+            StubDataclassDerivedRecord(id="abc3"),
+            StubDataclassDerivedFromDerivedRecord(id="abc4"),
+            StubDataclassOtherDerivedRecord(id="abc5"),
+            StubDataclassListFields(id="abc6"),
+            StubDataclassOptionalFields(id="abc7"),
+            StubDataclassDictFields(id="abc8"),
+            StubDataclassDictListFields(id="abc9"),
+            StubDataclassListDictFields(id="abc10"),
+            StubDataclassPrimitiveFields(key_str_field="abc11"),
+        ]
+
+        sample_keys = [x.get_key() for x in samples]
+
+        # Load from empty tables
+        loaded_records = [context.load_one(type(key), key, is_record_optional=True) for key in sample_keys]
+        assert loaded_records == [None] * len(samples)
+
+        # Populate tables
+        context.save_many(samples)
+
+        # Load one by one for all keys because each type is different
+        loaded_records = [context.load_one(type(key), key) for key in sample_keys]
+        assert loaded_records == samples
+
+        # Delete first and last record
+        context.delete_many([sample_keys[0], sample_keys[-1]])
+        loaded_records = [context.load_one(type(key), key, is_record_optional=True) for key in sample_keys]
+        assert loaded_records == [None, *samples[1:-1], None]
+
+        # Delete all records
+        context.delete_many(sample_keys)
+        loaded_records = [context.load_one(type(key), key, is_record_optional=True) for key in sample_keys]
+        assert loaded_records == [None] * len(samples)
+
+
+def test_record_upsert():
+    """Check that an existing entry is overridden when a new entry with the same key is saved."""
+
+    db_class = ClassInfo.get_class_path(BasicMongoDb)
+    with TestingContext(db_class=db_class) as context:
+        # create sample and save
+        sample = StubDataclassRecord()
+        context.save_one(sample)
+        loaded_record = context.load_one(StubDataclassRecord, sample.get_key())
+        assert loaded_record == sample
+
+        # create sample with the same key and save
+        override_sample = StubDataclassDerivedRecord()
+        context.save_one(override_sample)
+        loaded_record = context.load_one(StubDataclassDerivedRecord, sample.get_key())
+        assert loaded_record == override_sample
+
+        override_sample = StubDataclassDerivedFromDerivedRecord()
+        context.save_one(override_sample)
+        loaded_record = context.load_one(StubDataclassDerivedFromDerivedRecord, sample.get_key())
+        assert loaded_record == override_sample
+
+
+def test_load_all():
+    """Test 'load_all' method."""
+
+    db_class = ClassInfo.get_class_path(BasicMongoDb)
+    with TestingContext(db_class=db_class) as context:
+        base_samples = [
+            StubDataclassRecord(id="base1"),
+            StubDataclassRecord(id="base2"),
+            StubDataclassRecord(id="base3"),
+        ]
+
+        derived_samples = [
+            StubDataclassDerivedRecord(id="derived1"),
+            StubDataclassDerivedFromDerivedRecord(id="derived2"),
+        ]
+
+        other_derived_samples = [
+            StubDataclassOtherDerivedRecord(id="derived3"),
+        ]
+
+        all_samples = base_samples + derived_samples + other_derived_samples
+
+        context.save_many(all_samples)
+
+        loaded_records = context.load_all(StubDataclassRecord)
+        assert _assert_equals_iterable_without_ordering(all_samples, loaded_records)
+
+        loaded_records = context.load_all(StubDataclassDerivedRecord)
+        assert _assert_equals_iterable_without_ordering(derived_samples, loaded_records)
+
+
+@pytest.mark.skip("Performance test.")
+def test_performance():
+    """Test performance of save/load methods."""
+
+    db_class = ClassInfo.get_class_path(BasicMongoDb)
+    with TestingContext(db_class=db_class) as context:
+        n = 1000
+        samples = [StubDataclassPrimitiveFields(key_str_field=f"key{i}") for i in range(n)]
+        sample_keys = [sample.get_key() for sample in samples]
+
+        print(f">>> Test stub type: {StubDataclassPrimitiveFields.__name__}, {n=}.")
+        start_time = time.time()
+        context.save_many(samples)
+        end_time = time.time()
+
+        print(f"Save many bulk: {end_time - start_time}s.")
+
+        start_time = time.time()
+        for sample in samples:
+            context.save_one(sample)
+        end_time = time.time()
+        print(f"Save many one by one: {end_time - start_time}s.")
+
+        start_time = time.time()
+        list(context.load_many(sample_keys))
+        end_time = time.time()
+        print(f"Load many bulk: {end_time - start_time}s.")
+
+        start_time = time.time()
+        for key in sample_keys:
+            context.load_one(type(key), key)
+        end_time = time.time()
+        print(f"Load many one by one: {end_time - start_time}s.")
+
+
+def test_singleton():
+    """Test singleton type saving."""
+
+    db_class = ClassInfo.get_class_path(BasicMongoDb)
+    with TestingContext(db_class=db_class) as context:
+        singleton_sample = StubDataclassSingleton()
+        context.save_one(singleton_sample)
+        loaded_sample = context.load_one(StubDataclassSingleton, singleton_sample.get_key())
+        assert loaded_sample == singleton_sample
+
+        other_singleton_sample = StubDataclassSingleton(str_field="other")
+        context.save_one(other_singleton_sample)
+        all_records = list(context.load_all(other_singleton_sample.__class__))
+        assert len(all_records) == 1
+        assert all_records[0] == other_singleton_sample
+
+
 def test_check_db_id():
     """Test '_get_db_name' method."""
 
@@ -65,7 +290,6 @@ def test_check_db_id():
             BasicMongoDb.check_db_id("abc/xyz")
 
 
-@pytest.mark.skip("Requires MongoDB server.")  # TODO: Switch test to MongoMock
 def test_load_filter():
     """Test 'load_filter' method."""
 
@@ -73,39 +297,15 @@ def test_load_filter():
     with TestingContext(db_class=db_class) as context:
         # Create test record and populate with sample data
         offset = 0
-        matching_records = [StubDataclassDerivedRecord(id=str(offset + i), derived_field="a") for i in range(2)]
+        matching_records = [StubDataclassDerivedRecord(id=str(offset + i), derived_str_field="a") for i in range(2)]
         offset = len(matching_records)
-        non_matching_records = [StubDataclassDerivedRecord(id=str(offset + i), derived_field="b") for i in range(2)]
+        non_matching_records = [StubDataclassDerivedRecord(id=str(offset + i), derived_str_field="b") for i in range(2)]
         context.save_many(matching_records + non_matching_records)
 
-        filter_obj = StubDataclassDerivedRecord(id=None, derived_field="a")
+        filter_obj = StubDataclassDerivedRecord(id=None, derived_str_field="a")
         loaded_records = context.load_filter(StubDataclassDerivedRecord, filter_obj)
         assert len(loaded_records) == len(matching_records)
         assert all(x.derived_str_field == filter_obj.derived_str_field for x in loaded_records)
-
-
-@pytest.mark.skip("Requires MongoDB server.")  # TODO: Switch test to MongoMock
-def test_smoke():
-    """Smoke test."""
-
-    # TODO: Do not hardcode DB name
-    db_class = ClassInfo.get_class_path(BasicMongoDb)
-    with TestingContext(db_class=db_class) as context:
-        # Create test record and populate with sample data
-        record = StubDataclassRecord()
-        key = record.get_key()
-
-        # Save a single record
-        context.save_many([record])
-
-        # Load using record or key
-        loaded_records = context.load_many(StubDataclassRecord, [record, key, None])
-        assert loaded_records[0] is record  # Same object is returned without lookup
-        assert loaded_records[1] == record  # Not the same object but equal
-        assert loaded_records[2] is None
-
-        assert context.load_one(StubDataclassRecord, record) is record  # Same object is returned without lookup
-        assert context.load_one(StubDataclassRecord, key) == record  # Not the same object but equal
 
 
 if __name__ == "__main__":
