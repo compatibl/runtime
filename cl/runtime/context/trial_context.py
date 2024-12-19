aa169cfc805bb33e1b4edd3a8eff307934cd88ee
Alexander Sokol <sokol@compatibl.com>
2024-12-18 19:19:22 -0500

    Use get_trial_id(...) to calculate composite trial_id from the TrialContext stack.
    (cherry picked from commit 0d8612e7335ad31fa016bf33538bdde0e3aaff80)

--- cl/runtime/context/trial_context.py
+++ cl/runtime/context/trial_context.py
@@ -23,21 +23,14 @@ class TrialContext(BaseContext):
     """Context for a single trial in an experiment."""
 
     trial_id: str | None = None
-    """Trial identifier specified by this context, or None if not running a trial."""
-
-    def __post_init__(self):
-        """Combine with the value from the previous trial context using dot delimiter."""
-
-        # Do not execute this code on deserialized context instances (e.g. when they are passed to a task queue)
-        if not self.is_deserialized:
-
-            # Combine with the previous trial_id if set
-            if (prev_context := TrialContext.current_or_none()) is not None and prev_context.trial_id is not None:
-                if self.trial_id is not None:
-                    self.trial_id = f"{prev_context.trial_id}.{self.trial_id}"
-                else:
-                    self.trial_id = prev_context.trial_id
-
+    """
+    Trial identifier specified by this context. 
+    
+    Notes:
+      - Because 'with' clause cannot be under if/else, in some cases trial_id may be conditionally None
+        but 'with TrialContext(...)' clause would still be present.
+      - If trial_id is None, this TrialContext is disregarded
+    """
     
     @classmethod
     def get_context_type(cls) -> str:
@@ -50,3 +43,25 @@ class TrialContext(BaseContext):
         """
         return "TrialContext"
 
+    @classmethod
+    def get_trial_id(cls) -> str | None:
+        """
+        Unique trial identifier in backslash-delimited format obtained by concatenating identifiers from
+        the TrialContext stack in the order entered, or None outside 'with TrialContext(...)' clause.
+
+        Notes:
+          - Because 'with' clause cannot be under if/else, in some cases trial_id may be conditionally None
+            but 'with TrialContext(...)' clause would still be present.
+          - If trial_id is None, the corresponding TrialContext is disregarded
+          - If trial_id is None for the entire the TrialContext stack, this method returns None
+        """
+        if cls.current_or_none() is not None:
+            # Gather those tokens from the context stack that are not None
+            tokens = [trial_id for context in cls._get_context_stack() if (trial_id := context.trial_id) is not None]
+            # Consider the possibility that after removing tokens that are None, the list becomes empty
+            if tokens:
+                return "\\".join(tokens)  # Concatenate
+            else:
+                return None
+        else:
+            return None
