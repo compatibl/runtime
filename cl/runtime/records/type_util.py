4697d2ce7394596f74833a5eff02598cf0a853a2
Alexander Sokol <sokol@compatibl.com>
2024-12-29 21:46:49 -0500

    Add TypeUtil helper class and test.
    (cherry picked from commit a0a6f55f1f1414fdcbaa1b35fb360703adf092fa)

---
+++ cl/runtime/records/type_util.py
@@ -0,0 +1,73 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Any
+from typing import Type
+
+
+class TypeUtil:
+    """Helper class for type checking."""
+
+    @classmethod
+    def check_same_type(
+        cls,
+        instance_or_type: Any,
+        expected_type: Type,
+        *,
+        name: str | None = None,
+    ) -> None:
+        """
+        Raises an error if the type of 'actual_instance_or_type' is not 'expected_type', subclasses are not permitted.
+
+        Notes:
+            Use 'check_subclass' method to allow subclasses to pass the check.
+
+        Args:
+            instance_or_type: Actual object or type
+            expected_type: Expected type
+            name: Lowercase name of parameter or field (optional)
+        """
+        # If not a type, assume it is an instance
+        actual_type = instance_or_type if isinstance(instance_or_type, type) else type(instance_or_type)
+        if not (actual_type is expected_type):
+            # Use default description if the name is not provided
+            what = f"The value for {name}" if name else "Parameter or field"
+            actual = actual_type.__name__
+            expected = expected_type.__name__
+            raise RuntimeError(f"{what} has type {actual} where {expected} is expected (subclasses are not permitted).")
+
+    @classmethod
+    def check_subclass(
+        cls,
+        instance_or_type: Any,
+        expected_type: Type,
+        *,
+        name: str | None = None,
+    ) -> None:
+        """
+        Raises an error if the type of 'actual_instance_or_type' is not the same type or subclass of 'expected_type'.
+
+        Args:
+            instance_or_type: Actual object or type
+            expected_type: Expected type
+            name: Lowercase name of parameter or field (optional)
+        """
+        # If not a type, assume it is an instance
+        actual_type = instance_or_type if isinstance(instance_or_type, type) else type(instance_or_type)
+        if not issubclass(actual_type, expected_type):
+            # Use default description if the name is not provided
+            what = f"The value for {name}" if name else "Parameter or field"
+            actual = actual_type.__name__
+            expected = expected_type.__name__
+            raise RuntimeError(f"{what} has type {actual} where {expected} or its a subclass is expected.")
---
+++ tests/cl/runtime/records/test_type_util.py
@@ -0,0 +1,45 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+
+from cl.runtime.records.type_util import TypeUtil
+from stubs.cl.runtime import StubDataclassRecord, StubDataclassDerivedRecord
+
+
+def test_check_same_type():
+    """Test for TypeCheck.check_same_type method."""
+    TypeUtil.check_same_type(int, int)
+    TypeUtil.check_same_type(StubDataclassRecord, StubDataclassRecord)
+    with pytest.raises(Exception):
+        TypeUtil.check_same_type(int, float)
+    with pytest.raises(Exception):
+        TypeUtil.check_same_type(int, StubDataclassRecord)
+    with pytest.raises(Exception):
+        TypeUtil.check_same_type(StubDataclassDerivedRecord, StubDataclassRecord)
+
+
+def test_check_subclass():
+    """Test for TypeCheck.check_subclass method."""
+    TypeUtil.check_subclass(int, int)
+    TypeUtil.check_subclass(StubDataclassRecord, StubDataclassRecord)
+    TypeUtil.check_subclass(StubDataclassDerivedRecord, StubDataclassRecord)
+    with pytest.raises(Exception):
+        TypeUtil.check_subclass(int, float)
+    with pytest.raises(Exception):
+        TypeUtil.check_subclass(int, StubDataclassRecord)
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
