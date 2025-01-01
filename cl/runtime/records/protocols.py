09d69e12920d19f50a6f28c1d0f5f974630d8f84
Alexander Sokol <sokol@compatibl.com>
2025-01-01 02:40:36 -0500

    Remove unused protocols, add is_key_or_record method.
    (cherry picked from commit d4ed3b9fdcb0b27d5e50e3b780d8633c8d73b368)

--- cl/runtime/records/protocols.py
+++ cl/runtime/records/protocols.py
@@ -42,9 +42,6 @@ TKeyField = Dict[str, "TKeyField"] | TPrimitive | Enum
 TKeyDict = Dict[str, TKeyField]
 """Serialized key in dictionary format."""
 
-TStamp = dt.datetime | UUID | None
-"""Timestamp or time-ordered globally unique identifier in UUID7 format."""  # TODO: Confirm UUID format to use
-
 TQuery = Tuple[
     Type,  # Query type and its descendents will be returned by the query. It must include all query and order fields.
     Dict[str, Any],  # NoSQL query conditions in MongoDB format.
@@ -85,36 +82,24 @@ class InitProtocol:
         """Similar to __init__ but can use fields set after construction."""
 
 
-class ValidateProtocol:
-    """Protocol implemented by objects that support validation."""
-
-    def validate(self) -> None:
-        """Confirm that previously set fields correspond to a valid object state."""
-
-
 def is_record(type_or_obj: Any) -> TypeGuard[RecordProtocol]:
-    """Check if type or object is a key (supports RecordProtocol) based on the presence of 'get_key' attribute."""
+    """Check if type or object is a record based on the presence of 'get_key' method."""
     return hasattr(type_or_obj, "get_key")
 
 
-def is_key(type_or_obj: Any) -> bool:
-    """
-    Check if type or object is a key (supports KeyProtocol) but not a record (does not support RecordProtocol)
-    based on the presence of 'get_key_type' attribute and the absence of 'get_key' attribute.
-    """
-    return hasattr(type_or_obj, "get_key_type") and not hasattr(type_or_obj, "get_key")  # TODO: Check if abstract
+def is_key_or_record(type_or_obj: Any) -> TypeGuard[KeyProtocol]:
+    """Check if type or object is a key or record based on the presence of 'get_key_type' method."""
+    return hasattr(type_or_obj, "get_key_type")
 
 
-def is_key_guard(type_or_obj: Any) -> TypeGuard[KeyProtocol]:
-    """Same as is_key but returns a type guard."""
-    return is_key(type_or_obj)
+def is_key(type_or_obj: Any) -> TypeGuard[KeyProtocol]:
+    """
+    Check if type or object is a key but not a record based on the presence of 'get_key_type' method and the
+    absence of 'get_key' method. Consider using a faster alternative 'is_key_or_record' if possible.
+    """
+    return hasattr(type_or_obj, "get_key_type") and not hasattr(type_or_obj, "get_key")
 
 
 def has_init(type_or_obj: Any) -> TypeGuard[InitProtocol]:
     """Check if type or object requires initialization (InitProtocol) based on the presence of 'init' attribute."""
     return hasattr(type_or_obj, "init")
-
-
-def has_validate(type_or_obj: Any) -> TypeGuard[ValidateProtocol]:
-    """Check if type or object supports validation (ValidateProtocol) based on the presence of 'validate' attribute."""
-    return hasattr(type_or_obj, "validate")
