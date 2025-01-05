721f0687a11368341af82f9169dffa76006d252b
Alexander Sokol <sokol@compatibl.com>
2025-01-05 12:41:55 -0500

    Switch to a single standardized test for type declaration format.
    (cherry picked from commit 5e15638e0fd51ba4e92a82ce3d510352e2e66116)

--- tests/cl/runtime/schema/test_type_decl.py
+++ tests/cl/runtime/schema/test_type_decl.py
@@ -13,75 +13,75 @@
 # limitations under the License.
 
 import pytest
+from dataclasses import asdict
 from cl.runtime.backend.core.ui_app_state import UiAppState
 from cl.runtime.primitive.case_util import CaseUtil
+from cl.runtime.records.type_util import TypeUtil
 from cl.runtime.schema.handler_declare_block_decl import HandlerDeclareBlockDecl
 from cl.runtime.schema.type_decl import TypeDecl
+from cl.runtime.serialization.dict_serializer import DictSerializer
+from cl.runtime.testing.regression_guard import RegressionGuard
+from stubs.cl.runtime import StubDataclassComposite
+from stubs.cl.runtime import StubDataclassDerivedFromDerivedRecord
+from stubs.cl.runtime import StubDataclassDerivedRecord
+from stubs.cl.runtime import StubDataclassDictFields
+from stubs.cl.runtime import StubDataclassDictListFields
+from stubs.cl.runtime import StubDataclassListDictFields
+from stubs.cl.runtime import StubDataclassListFields
+from stubs.cl.runtime import StubDataclassNestedFields
+from stubs.cl.runtime import StubDataclassOptionalFields
+from stubs.cl.runtime import StubDataclassOtherDerivedRecord
+from stubs.cl.runtime import StubDataclassPrimitiveFields
 from stubs.cl.runtime import StubDataclassRecord
-from stubs.cl.runtime.records.for_dataclasses.stub_dataclass_derived_record import StubDataclassDerivedRecord
+from stubs.cl.runtime import StubDataclassSingleton
+from stubs.cl.runtime import StubHandlers
+from stubs.cl.runtime.records.for_dataclasses.stub_dataclass_aliased_record import StubDataclassAliasedRecord
+from stubs.cl.runtime.records.for_dataclasses.stub_dataclass_final_key import StubDataclassFinalKey
+from stubs.cl.runtime.records.for_dataclasses.stub_dataclass_final_record import StubDataclassFinalRecord
+from stubs.cl.runtime.records.for_dataclasses.stub_dataclass_nested_final_record import StubDataclassNestedFinalRecord
+from stubs.cl.runtime.records.for_dataclasses.stub_dataclass_tuple_fields import StubDataclassTupleFields
+from stubs.cl.runtime.records.for_dataclasses.stub_dataclass_versioned_record import StubDataclassVersionedRecord
 
-type_decl_dict = {
-    "Module": {"ModuleName": "Cl.Runtime.Backend.Core"},
-    "Name": "UiAppState",
-    "DisplayKind": "Basic",
-    "Elements": [
-        {"Value": {"Type": "String"}, "Name": "User", "Optional": True},
-    ],
-    "Keys": ["User"],
-}
-
-
-class TestClass:
-
-    def not_a_handler_method(self):
-        pass
-
-    def run_is_a_handler_method(self):
-        pass
-
-
-def to_snake_case(data):
-    if isinstance(data, dict):
-        return {
-            CaseUtil.pascal_to_snake_case(key) if isinstance(key, str) else key: to_snake_case(value)
-            for key, value in data.items()
-        }
-    elif isinstance(data, list):
-        return [to_snake_case(item) for item in data]
-    else:
-        return data
-
-
-def test_to_type_decl_dict():
-    """Test TypeDecl.to_type_decl_dict method."""
-
-    record_types = [UiAppState, StubDataclassRecord]
-    for record_type in record_types:
-        type_decl = TypeDecl.for_type(record_type)
-        type_decl_dict_ = type_decl.to_type_decl_dict()
-
-        assert isinstance(type_decl_dict_, dict), f"Expected type: Dict, but got {type(type_decl_dict_)}"
-        assert len(type_decl_dict_) > 0, f"Result dictionary is empty!"
+_SAMPLE_TYPES = [
+    UiAppState,
+    StubDataclassRecord,
+    StubDataclassNestedFields,
+    StubDataclassComposite,
+    StubDataclassDerivedRecord,
+    StubDataclassDerivedFromDerivedRecord,
+    StubDataclassOtherDerivedRecord,
+    StubDataclassListFields,
+    # TODO: Fix StubDataclassTupleFields,
+    StubDataclassOptionalFields,
+    # TODO: StubDataclassDictFields,
+    # TODO: StubDataclassDictListFields,
+    StubDataclassListDictFields,
+    StubDataclassPrimitiveFields,
+    StubDataclassSingleton,
+    StubDataclassAliasedRecord,
+    StubHandlers,
+    StubDataclassRecord,
+    StubDataclassVersionedRecord,
+    StubDataclassFinalKey,
+    StubDataclassFinalRecord,
+    StubDataclassNestedFinalRecord,
+]
 
 
-def test_type_without_handlers():
-    type_decl = TypeDecl.for_type(StubDataclassDerivedRecord)
+def test_type_decl():
+    """Test type decls generated for stub records."""
+    for record_type in _SAMPLE_TYPES:
 
-    assert type_decl.name == "StubDataclassDerivedRecord"
-    assert type_decl.comment == "Stub derived class."
-    assert type_decl.declare is None, f"Class: {StubDataclassDerivedRecord} should not have handlers!"
+        # Get type declaration
+        type_name = TypeUtil.name(record_type)
+        type_decl: TypeDecl = TypeDecl.for_type(record_type)
+        assert type_decl is not None
 
-
-def test_type_with_handlers():
-    type_decl: TypeDecl = TypeDecl.for_type(TestClass)
-
-    assert type_decl is not None
-
-    handler_decl: HandlerDeclareBlockDecl = type_decl.declare
-    assert handler_decl is not None
-
-    handlers = handler_decl.handlers
-    assert len(handlers) == 1, f"One handler expected!"
+        # Use native dataclasses.asdict to ensure we do not rely on the functionality being tested for serialization
+        type_decl_dict = asdict(type_decl)  # noqa
+        guard = RegressionGuard(channel=type_name)
+        guard.write(type_decl_dict)
+    RegressionGuard().verify_all()
 
 
 if __name__ == "__main__":
