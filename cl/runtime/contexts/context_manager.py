ba4266a2aca0d361b125a63e93c785ee9da66871
Alexander Sokol <sokol@compatibl.com>
2025-01-14 19:47:16 -0500

    Use BuildWhatEnum.DESERIALIZED parameter for DB load methods and ContextManager.
    (cherry picked from commit de01b9d2d18797fa93a4e2f8e8952e2360634bb5)

--- cl/runtime/contexts/context_manager.py
+++ cl/runtime/contexts/context_manager.py
@@ -19,6 +19,7 @@ from typing import List
 from typing_extensions import Self
 from cl.runtime.contexts.context import _CONTEXT_STACK_DICT_VAR
 from cl.runtime.contexts.context import Context
+from cl.runtime.records.build_what_enum import BuildWhatEnum
 from cl.runtime.serialization.dict_serializer import DictSerializer
 
 _DICT_SERIALIZER = DictSerializer()
@@ -59,8 +60,9 @@ class ContextManager:
                         f"Context {type(context).__name__} cannot be activated by ContextManager "
                         f"because it is not derived from {Context.__name__}."
                     )
-                # Build
-                context.build()
+                # Build as deserialized record, init implementations will use this parameter
+                # to avoid overriding deserialized public fields
+                context.build(what=BuildWhatEnum.DESERIALIZED)
 
     def __enter__(self) -> Self:
         """Invoke __enter__ for each item in the 'contexts' field."""
