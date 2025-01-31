5647fad6ae2622ec50efc4d711a95639c4a4aaf4
Alexander Sokol <sokol@compatibl.com>
2025-01-30 21:22:26 -0500

    Move test of the build method to test_build_mixin.
    (cherry picked from commit 35261054146f512e52b0b3eddd1497661abf21cf)

--- tests/cl/runtime/records/test_build_mixin.py
+++ tests/cl/runtime/records/test_build_mixin.py
@@ -15,6 +15,7 @@
 import pytest
 from dataclasses import dataclass
 from cl.runtime.records.for_dataclasses.freezable import Freezable
+from cl.runtime.testing.regression_guard import RegressionGuard
 
 
 @dataclass(slots=True, kw_only=True)
@@ -22,11 +23,62 @@ class Base(Freezable):
     _protected_base_field: str | None = None
     public_base_field_1: str | None = None
     public_base_field_2: str | None = None
+    def __init(self) -> None:
+        """Use instead of __init__ in the builder pattern, invoked by the build method in base to derived order."""
+        RegressionGuard().write("> Base.init")
 
 
 @dataclass(slots=True, kw_only=True)
 class Derived(Base):
     public_derived_field: str | None = None
+    def __init(self) -> None:
+        """Use instead of __init__ in the builder pattern, invoked by the build method in base to derived order."""
+        RegressionGuard().write(">> Derived.init")
+
+
+@dataclass(slots=True, kw_only=True)
+class DerivedFromDerivedWithInit(Derived):
+    def __init(self) -> None:
+        """Use instead of __init__ in the builder pattern, invoked by the build method in base to derived order."""
+        RegressionGuard().write(">>> DerivedFromDerivedWithInit.init")
+
+
+@dataclass(slots=True, kw_only=True)
+class DerivedFromDerivedWithoutInit(Derived):
+    pass
+
+
+@dataclass(slots=True, kw_only=True)
+class _OneLeadingUnderscore(Freezable):
+    def __init(self) -> None:
+        """Use instead of __init__ in the builder pattern, invoked by the build method in base to derived order."""
+        RegressionGuard().write("> _OneLeadingUnderscore.init")
+
+
+@dataclass(slots=True, kw_only=True)
+class __TwoLeadingUnderscores(Freezable):
+    def __init(self) -> None:
+        """Use instead of __init__ in the builder pattern, invoked by the build method in base to derived order."""
+        RegressionGuard().write("> __TwoLeadingUnderscores.init")
+
+
+def test_build():
+    """Test BuildMixin.build method."""
+
+    guard = RegressionGuard()
+    guard.write("Testing Base:")
+    Base().build()
+    guard.write("Testing Derived:")
+    Derived().build()
+    guard.write("Testing DerivedFromDerivedWithInit:")
+    DerivedFromDerivedWithInit().build()
+    guard.write("Testing DerivedFromDerivedWithoutInit:")
+    DerivedFromDerivedWithoutInit().build()
+    guard.write("Testing _OneLeadingUnderscore:")
+    _OneLeadingUnderscore().build()
+    guard.write("Testing __TwoLeadingUnderscores:")
+    __TwoLeadingUnderscores().build()
+    RegressionGuard().verify_all()
 
 
 def test_clone():
