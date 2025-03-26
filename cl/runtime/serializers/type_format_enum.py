9efe8e276ca1b90d52ff7974e1256648440401a6
Alexander Sokol <sokol@compatibl.com>
2025-03-26 10:50:54 -0400

    Add two enums governing type inclusion in serialized data.
    (cherry picked from commit 6bf2ed751bcc9c78db7fc0efcc74df2b9ef3d071)

---
+++ cl/runtime/serializers/type_format_enum.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from enum import IntEnum
+from enum import auto
+
+
+class TypeFormatEnum(IntEnum):
+    """Format of the type information in serialized data."""
+
+    TYPE_NAME = auto()
+    """Unique type alias alias or ClassName without the module if type alias is not provided."""
+
+    IMPORT_PATH = auto()
+    """Import path inclusive of module using Python conventions (e.g., module.ClassName)."""
---
+++ cl/runtime/serializers/type_inclusion_enum.py
@@ -0,0 +1,29 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from enum import IntEnum
+from enum import auto
+
+
+class TypeInclusionEnum(IntEnum):
+    """Where to include type information in serialized data."""
+
+    DEFAULT = auto()
+    """Include type information in serialized data at root level always, and at other levels for derived types only."""
+
+    NEVER = auto()
+    """Do not include type information in serialized data, deserialization is not possible."""
+
+    ALWAYS = auto()
+    """Always include type information in serialized data at all levels."""
