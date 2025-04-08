95d932aacaabc8f6a453f8687d8591c63e75d053
Alexander Sokol <sokol@compatibl.com>
2025-04-07 22:42:49 -0400

    Add ReportingSerializer with JSON and YAML encoder options.
    (cherry picked from commit 1c6e56d3aed90bfd2ef8acab5eb385908d8e2658)

---
+++ cl/runtime/serializers/reporting_serializer.py
@@ -0,0 +1,119 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from dataclasses import dataclass
+from enum import Enum
+from typing import Any
+from cl.runtime.primitive.case_util import CaseUtil
+from cl.runtime.records.for_dataclasses.extensions import required
+from cl.runtime.records.protocols import MAPPING_CLASS_NAMES
+from cl.runtime.records.protocols import PRIMITIVE_CLASS_NAMES
+from cl.runtime.records.protocols import SEQUENCE_CLASS_NAMES
+from cl.runtime.records.protocols import is_data
+from cl.runtime.records.type_util import TypeUtil
+from cl.runtime.schema.type_hint import TypeHint
+from cl.runtime.serializers.encoder import Encoder
+from cl.runtime.serializers.serializer import Serializer
+from cl.runtime.serializers.slots_util import SlotsUtil
+
+
+@dataclass(slots=True, kw_only=True)
+class ReportingSerializer(Serializer):
+    """Unidirectional serialization of object to a dictionary without type information."""
+
+    primitive_serializer: Serializer = required()
+    """Use to serialize primitive types."""
+
+    enum_serializer: Serializer = required()
+    """Use to serialize enum types."""
+
+    encoder: Encoder | None = None
+    """Use to encode the output of serialize method if specified."""
+
+    pascalize_keys: bool | None = None
+    """Pascalize keys during serialization if set."""
+
+    def serialize(self, data: Any, type_hint: TypeHint | None = None) -> Any:
+        """Serialize data to a dictionary."""
+
+        # Serialize data to a dictionary without encoding
+        serialized = self._serialize(data, type_hint)
+
+        # Encode the result if an encoder is provided
+        if self.encoder:
+            return self.encoder.encode(serialized)
+        else:
+            return serialized
+
+    def _serialize(self, data: Any, type_hint: TypeHint | None = None) -> Any:
+        """Serialize data to a dictionary without encoding."""
+
+        # TODO: Support type hint for validation only
+
+        if data is None:
+            # Pass through None for untyped serialization
+            return None
+        elif (data_class_name := TypeUtil.name(data)) in PRIMITIVE_CLASS_NAMES:
+            # Primitive type, serialize using primitive serializer if specified, otherwise return raw data
+            return self.primitive_serializer.serialize(data)
+        elif data_class_name in SEQUENCE_CLASS_NAMES:
+            # Sequence container, including items that are None in output to preserve item positions
+            result = [
+                (
+                    None
+                    if v is None
+                    else (
+                        self.primitive_serializer.serialize(v)
+                        if v.__class__.__name__ in PRIMITIVE_CLASS_NAMES
+                        else (self.enum_serializer.serialize(v) if isinstance(v, Enum) else self._serialize(v))
+                    )
+                )
+                for v in data
+            ]
+            return result
+        elif data_class_name in MAPPING_CLASS_NAMES:
+            # Mapping container, do not include values that are None
+            result = {
+                (k if not self.pascalize_keys else CaseUtil.snake_to_pascal_case(k)): (
+                    self.primitive_serializer.serialize(v)
+                    if v.__class__.__name__ in PRIMITIVE_CLASS_NAMES
+                    else (self.enum_serializer.serialize(v) if isinstance(v, Enum) else self._serialize(v))
+                )
+                for k, v in data.items()
+            }
+            return result
+        elif isinstance(data, Enum):
+            # Enum type, serialize using enum serializer if specified, otherwise return raw data
+            return self.enum_serializer.serialize(data)
+        elif is_data(data):
+            # Slotted class, get slots from this class and its bases in the order of declaration from base to derived
+            slots = SlotsUtil.get_slots(type(data))
+            # Serialize slot values in the order of declaration except those that are None
+            result = {
+                (k if not self.pascalize_keys else CaseUtil.snake_to_pascal_case(k)): (
+                    self.primitive_serializer.serialize(v)
+                    if v.__class__.__name__ in PRIMITIVE_CLASS_NAMES
+                    else (self.enum_serializer.serialize(v) if isinstance(v, Enum) else self._serialize(v))
+                )
+                for k in slots
+                if (v := getattr(data, k)) is not None and not k.startswith("_")
+            }
+            return result
+        else:
+            # Did not match a supported data type
+            raise RuntimeError(f"Cannot serialize data of type '{type(data)}'.")
+
+    def deserialize(self, data: Any, type_hint: TypeHint | None = None) -> Any:
+        """Deserialize a dictionary into object using type information extracted from the _type field."""
+        raise RuntimeError(f"{TypeUtil.name(self)} does not support deserialization.")
---
+++ cl/runtime/serializers/yaml_encoder.py
@@ -0,0 +1,152 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime as dt
+from dataclasses import dataclass
+from io import StringIO
+from typing import Any
+from uuid import UUID
+from ruamel.yaml import YAML
+from ruamel.yaml import StringIO
+from ruamel.yaml.constructor import SafeConstructor
+from ruamel.yaml.nodes import MappingNode
+from ruamel.yaml.nodes import ScalarNode
+from ruamel.yaml.nodes import SequenceNode
+from cl.runtime.records.for_dataclasses.extensions import required
+from cl.runtime.records.protocols import PRIMITIVE_CLASS_NAMES
+from cl.runtime.schema.type_hint import TypeHint
+from cl.runtime.serializers.data_serializer import DataSerializer
+from cl.runtime.serializers.encoder import Encoder
+from cl.runtime.serializers.enum_serializers import EnumSerializers
+from cl.runtime.serializers.primitive_serializers import PrimitiveSerializers
+from cl.runtime.serializers.serializer import Serializer
+from cl.runtime.serializers.type_format import TypeFormat
+from cl.runtime.serializers.type_hints import TypeHints
+from cl.runtime.serializers.type_inclusion import TypeInclusion
+
+# Use primitive serializer with default settings to serialize all primitive types to string
+_PRIMITIVE_SERIALIZER = PrimitiveSerializers.DEFAULT
+
+
+def str_representer(dumper, data):
+    """Configure YAML class for serializing a str field."""
+    data_str = _PRIMITIVE_SERIALIZER.serialize(data, TypeHints.STR_OR_NONE)
+    if data_str:
+        style = "|" if data_str and "\n" in data_str else None
+        return dumper.represent_scalar("tag:yaml.org,2002:str", data_str, style=style)
+    else:
+        return dumper.represent_scalar("tag:yaml.org,2002:null", None, style=None)
+
+
+def float_representer(dumper, data):
+    """Configure YAML class for serializing a float field."""
+    data_str = _PRIMITIVE_SERIALIZER.serialize(data, TypeHints.FLOAT_OR_NONE)
+    return dumper.represent_scalar("tag:yaml.org,2002:float", data_str, style=None)
+
+
+def time_representer(dumper, data):
+    """Configure YAML class for serializing a time field."""
+    data_str = _PRIMITIVE_SERIALIZER.serialize(data, TypeHints.TIME_OR_NONE)
+    return dumper.represent_scalar("tag:yaml.org,2002:str", data_str)
+
+
+def datetime_representer(dumper, data):
+    """Configure YAML class for serializing a datetime field."""
+    data_str = _PRIMITIVE_SERIALIZER.serialize(data, TypeHints.DATETIME_OR_NONE)
+    return dumper.represent_scalar("tag:yaml.org,2002:timestamp", data_str, style=None)
+
+
+def uuid_representer(dumper, data):
+    """Configure YAML class for serializing a UUID field."""
+    data_str = _PRIMITIVE_SERIALIZER.serialize(data, TypeHints.UUID_OR_NONE)
+    if data_str:
+        return dumper.represent_scalar("tag:yaml.org,2002:str", data_str, style=None)
+    else:
+        return dumper.represent_scalar("tag:yaml.org,2002:null", None, style=None)
+
+
+def bytes_representer(dumper, data):
+    """Configure YAML class for serializing a bytes field."""
+    data_str = _PRIMITIVE_SERIALIZER.serialize(data, TypeHints.BYTES_OR_NONE)
+    if data_str:
+        style = "|" if data_str and "\n" in data_str else None
+        return dumper.represent_scalar("tag:yaml.org,2002:str", data_str, style=style)
+    else:
+        return dumper.represent_scalar("tag:yaml.org,2002:null", None, style=None)
+
+
+# Roundtrip (typ=rt) style for the YAML writer is required to follow the formatting instructions in representers
+yaml_writer = YAML(typ="rt")
+
+# Add representers for writing the primitive types where YAML typ=rt format does not match our conventions
+yaml_writer.representer.add_representer(str, str_representer)
+yaml_writer.representer.add_representer(float, float_representer)
+yaml_writer.representer.add_representer(dt.datetime, datetime_representer)
+yaml_writer.representer.add_representer(dt.time, time_representer)
+yaml_writer.representer.add_representer(UUID, uuid_representer)
+yaml_writer.representer.add_representer(bytes, bytes_representer)
+
+
+class PrimitiveToStringConstructor(SafeConstructor):
+    """Custom constructor for YAML reader that ensures all primitive values are read as strings."""
+
+    def construct_object(self, node, deep=False):
+        """Convert from YAML types to native Python types."""
+        if isinstance(node, ScalarNode):  # , ScalarString, ScalarFloat, ScalarInt)):  # Primitive types
+            value = super().construct_scalar(node)
+            if isinstance(value, (int, float, bool)):
+                # Keep native Python types for int, float, bool
+                return value
+            else:
+                # Convert everything except None to string, pass through None
+                return str(value) if (value is not None and value != "") else None
+        elif isinstance(node, SequenceNode):  # , CommentedSeq)):  # Lists
+            return [self.construct_object(v, deep) for v in node.value]
+        elif isinstance(node, MappingNode):  # , CommentedMap)):  # Dictionaries
+            return {self.construct_object(k, deep): self.construct_object(v, deep) for k, v in node.value}
+        return super().construct_object(node, deep)
+
+
+# Override constructor in YAML reader for reading all types as strings
+yaml_reader = YAML(typ="safe")
+yaml_reader.Constructor = PrimitiveToStringConstructor
+
+
+@dataclass(slots=True, kw_only=True)
+class YamlEncoder(Encoder):
+    """Serialization without using the schema or retaining type information, not suitable for deserialization."""
+
+    def encode(self, data: Any) -> str | None:
+        """Encode to a string, pass through None."""
+
+        if data is None:
+            return ""
+        elif data.__class__.__name__ in PRIMITIVE_CLASS_NAMES:
+            # Use default primitive serializer for a primitive type
+            result = PrimitiveSerializers.DEFAULT.serialize(data)
+            # Add trailing newline
+            return f"{result}\n"
+        else:
+            # Use a YAML writer with custom representers for any structured data
+            output = StringIO()
+            yaml_writer.dump(data, output)
+            result = output.getvalue()
+            return result
+
+    def decode(self, data: str | None) -> Any:
+        """Decode from a string, pass through None."""
+
+        # Use a YAML reader with PrimitiveToStringConstructor to read all values as strings
+        result = yaml_reader.load(StringIO(data))
+        return result
