bd46abb4e4ab46119cd831d7651e3639e32e1391
Alexander Sokol <sokol@compatibl.com>
2025-07-10 04:42:14 -0400

    New field names for conditions with _op suffix.
    (cherry picked from commit 8872b6a6ea66e44a6465593d37f3b2979c3edd45)

--- cl/runtime/records/conditions.py
+++ cl/runtime/records/conditions.py
@@ -17,101 +17,134 @@ from typing import Generic
 from typing import Sequence
 from typing import Tuple
 from cl.runtime.records.bootstrap_mixin import BootstrapMixin
-from cl.runtime.records.protocols import TObj
+from cl.runtime.records.protocols import TObj, is_sequence, is_primitive
+from cl.runtime.records.type_util import TypeUtil
 
 
 class Condition(Generic[TObj], BootstrapMixin, ABC):
     """Common base class of all query conditions."""
 
+class Range(Generic[TObj], Condition[TObj]):
+    """Range with one or two bounds."""
 
-class Eq(Generic[TObj], Condition[TObj]):
-    """Matches when the argument is equal to the value, equivalent to specifying the value itself."""
+    __slots__ = ("gt_op", "gte_op", "lt_op", "lte_op",)
 
-    __slots__ = ("value",)
+    gt_op: TObj
+    """Value for greater-than operator."""
 
-    value: TObj
-    """Value to compare to."""
+    gte_op: TObj
+    """Value for greater-than-or-equal operator."""
 
-    def __init__(self, value: TObj):
-        """Create from the value."""
-        object.__setattr__(self, "value", value)
+    lt_op: TObj
+    """Value for less-than operator."""
+
+    lte_op: TObj
+    """Value for less-than-or-equal operator."""
+
+    def __init__(
+            self,
+            *,
+            gt: TObj | None = None,
+            gte: TObj | None = None,
+            lt: TObj | None = None,
+            lte: TObj | None = None,
+    ):
+        """Create from the individual conditions"""
+        self.gt_op = gt
+        self.gte_op = gte
+        self.lt_op = lt
+        self.lte_op = lte
 
 
 class And(Generic[TObj], Condition[TObj]):
     """Matches when all of the conditions match."""
 
-    __slots__ = ("conditions",)
+    __slots__ = ("and_op",)
 
-    conditions: Tuple[Condition[TObj] | TObj, ...]
-    """The sequence of conditions or values in AND."""
+    and_op: Tuple[Condition[TObj] | TObj, ...]
+    """The sequence of conditions or values in And operator."""
 
     def __init__(self, *args: Condition[TObj] | TObj):
         """Create from the sequence of conditions to match."""
-        object.__setattr__(self, "conditions", tuple(args))
+        self.and_op = tuple(args)
 
 
 class Or(Generic[TObj], Condition[TObj]):
     """Matches when at least one of the conditions matches."""
 
-    __slots__ = ("conditions",)
+    __slots__ = ("or_op",)
 
-    conditions: Tuple[Condition[TObj] | TObj, ...]
-    """The sequence of conditions or values in OR."""
+    or_op: Tuple[Condition[TObj] | TObj, ...]
+    """The sequence of conditions or values in Or operator."""
 
     def __init__(self, *args: Condition[TObj] | TObj):
         """Create from the sequence of conditions to match."""
-        object.__setattr__(self, "conditions", tuple(args))
+        self.or_op = tuple(args)
 
 
 class Not(Generic[TObj], Condition[TObj]):
     """Matches when the argument does not match and vice versa."""
 
-    __slots__ = ("condition",)
+    __slots__ = ("not_op",)
 
-    condition: Condition[TObj] | TObj
-    """Applies OR operator to the argument."""
+    not_op: Condition[TObj] | TObj
+    """Applies Not operator to the value."""
 
-    def __init__(self, condition: Condition | TObj):
+    def __init__(self, value: Condition | TObj):
         """Matches when the argument does not match and vice versa."""
-        object.__setattr__(self, "condition", condition)
+        self.not_op = value
 
 
 class Exists(Generic[TObj], Condition[TObj]):
     """Matches not None if true, matches None if false."""
 
-    __slots__ = ("value",)
+    __slots__ = ("exists_op",)
 
-    value: bool
+    exists_op: bool
     """Matches non-empty value if true, matches empty value if false."""
 
     def __init__(self, value: bool):
         """Matches non-empty value if true, matches empty value if false."""
-        object.__setattr__(self, "value", value)
+        if type(value) is bool:
+            self.exists_op = value
+        else:
+            raise RuntimeError(
+                f"Argument of Exists operator has type {TypeUtil.name(value)} which is not a bool.")
 
 
 class In(Generic[TObj], Condition[TObj]):
     """Matches when the argument is equal to one of the values."""
 
-    __slots__ = ("values",)
+    __slots__ = ("in_op",)
 
-    values: Tuple[TObj, ...]
+    in_op: Tuple[TObj, ...]
     """Values to compare to."""
 
     def __init__(self, values: Sequence[TObj]):
-        """Create from the sequence of values to compare to."""
-        if not isinstance(values, tuple):
-            object.__setattr__(self, "values", tuple(values))
+        """Create from the sequence of values to use with the In operator."""
+        if isinstance(values, tuple):
+            self.in_op = values
+        elif is_sequence(values):
+            self.in_op = tuple(values)
+        else:
+            raise RuntimeError(
+                f"Argument of In operator has type {TypeUtil.name(values)} which is not a sequence.")
 
 
 class NotIn(Generic[TObj], Condition[TObj]):
     """Matches when the argument is not equal to any of the values."""
 
-    __slots__ = ("values",)
+    __slots__ = ("nin_op",)
 
-    values: Tuple[TObj, ...]
+    nin_op: Tuple[TObj, ...]
     """Values to compare to."""
 
     def __init__(self, values: Sequence[TObj]):
-        """Create from the sequence of values to compare to."""
-        if not isinstance(values, tuple):
-            object.__setattr__(self, "values", tuple(values))
+        """Create from the sequence of values to use with the NotIn operator."""
+        if isinstance(values, tuple):
+            self.nin_op = values
+        elif is_sequence(values):
+            self.nin_op = tuple(values)
+        else:
+            raise RuntimeError(
+                f"Argument of NotIn operator has type {TypeUtil.name(values)} which is not a sequence.")
