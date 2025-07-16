79923434eb24f2ec397ec3d4ad6a0638a32e62ca
Alexander Sokol <sokol@compatibl.com>
2025-07-16 02:47:23 -0400

    Add QaSettings class and test.
    (cherry picked from commit 508c2f1df42406818f7adaa9844b90071f76f520)

---
+++ cl/runtime/settings/qa_settings.py
@@ -0,0 +1,40 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from dataclasses import dataclass
+from typing_extensions import final
+from cl.runtime.records.for_dataclasses.extensions import required
+from cl.runtime.records.protocols import is_sequence
+from cl.runtime.records.type_util import TypeUtil
+from cl.runtime.settings.project_settings import ProjectSettings
+from cl.runtime.settings.settings import Settings
+from cl.runtime.settings.settings_util import SettingsUtil
+
+
+@dataclass(slots=True, kw_only=True)
+@final
+class QaSettings(Settings):
+    """QA and unit test settings."""
+
+    qa_db_types: tuple[str, ...] | None = None
+    """Database type names for unit testing, at least one is required."""
+
+    def __init(self) -> None:
+        """Use instead of __init__ in the builder pattern, invoked by the build method in base to derived order."""
+        self.qa_db_types = SettingsUtil.to_str_tuple_or_none(
+            self.qa_db_types,
+            field_name="qa_db_types",
+            type_name=TypeUtil.name(self),
+        )
---
+++ tests/cl/runtime/settings/test_qa_settings.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+from cl.runtime.records.protocols import is_sequence
+from cl.runtime.settings.qa_settings import QaSettings
+
+
+def test_smoke():
+    """Test DbSettings class."""
+
+    qa_settings = QaSettings.instance()
+    assert qa_settings.qa_db_types is not None and is_sequence(qa_settings.qa_db_types)
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
