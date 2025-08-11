91035b6bdef555b815e142df8f77a2a77b925806
Alexander Sokol <sokol@compatibl.com>
2025-08-11 18:22:56 -0400

    Remove support for Python 3.10 from SlotsUtil.
    (cherry picked from commit 8c50eb9cd6539ca1f6db0efaf6ff0f7189345242)

--- cl/runtime/serializers/slots_util.py
+++ cl/runtime/serializers/slots_util.py
@@ -18,8 +18,11 @@ from typing import Tuple
 from memoization import cached
 from cl.runtime.records.type_util import TypeUtil
 
-_INHERITED_SLOTS = sys.version_info >= (3, 11)
-"""In Python 3.11+, __slots__ from base are included in __slots__ of descendants."""
+# We rely on __slots__ from base being included in __slots__ of descendants in Python 3.11+
+if sys.version_info < (3, 11):
+    raise RuntimeError(f"We rely on __slots__ from base being included in __slots__ of descendants.\n"
+                       f"This feature requires Python 3.11+, while this version is: "
+                       f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}")
 
 
 class SlotsUtil:
@@ -31,17 +34,11 @@ class SlotsUtil:
         """Return slots the order of declaration from base to derived."""
 
         if hasattr(data_type, "__slots__"):
-            # Traverse the class hierarchy from base to derived (reverse MRO order) collecting slots as specified
-            if _INHERITED_SLOTS:
-                # For v3.11 and later, __slots__ includes fields for this class only, use MRO to collect
-                # slots by traversing the entire class hierarchy, exclude None or empty __slots__ (both are falsy)
-                class_hierarchy_slots = [
-                    slots for base in reversed(data_type.__mro__) if (slots := getattr(base, "__slots__", None))
-                ]
-            else:
-                # Otherwise get slots from this type only
-                # Exclude None or empty __slots__ (both are falsy)
-                class_hierarchy_slots = [slots if (slots := getattr(data_type, "__slots__", None)) else tuple()]
+            # Use MRO to collect slots by traversing the entire class hierarchy,
+            # exclude None or empty __slots__ (both are falsy)
+            class_hierarchy_slots = [
+                slots for base in reversed(data_type.__mro__) if (slots := getattr(base, "__slots__", None))
+            ]
 
             # Exclude empty tuples and convert slots specified as a single string into tuple of size one
             class_hierarchy_slots = [(slots,) if isinstance(slots, str) else slots for slots in class_hierarchy_slots]
@@ -75,15 +72,5 @@ class SlotsUtil:
         and base in a way that works for Python 3.10 where slots are not inherited
         and Python 3.11+ where they are.
         """
-        if not _INHERITED_SLOTS:
-            # Slots from base must be combined with the current class slots,
-            # which must be present to avoid __dict__ but may be empty, e.g. ()
-            base_slots = getattr(base, "__slots__")
-            # __weakref__ should be explicitly filtered out(allowed only once in inheritance chain)
-            filtered_base_slots = tuple(s for s in base_slots if s != "__weakref__")
-            # Combine in the order of declaration from base to derived
-            result = tuple(filtered_base_slots + slots)
-        else:
-            # Slots are merged automatically
-            result = tuple(slots)
-        return result
+        # Slots are merged automatically
+        return tuple(slots)
