60b7f65e0f18c716cd63eadb495d7aaace292b56
Alexander Sokol <sokol@compatibl.com>
2025-08-23 11:33:19 -0400

    Refactor PrimitiveUtil with more detailed error messages.
    (cherry picked from commit bfc92ff7a8a5ebe4e67018111397c4338796ea00)

--- cl/runtime/primitive/primitive_util.py
+++ cl/runtime/primitive/primitive_util.py
@@ -15,86 +15,134 @@
 from typing import Any
 
 from cl.runtime.primitive.limits import check_int_32, check_int_54
+from cl.runtime.primitive.timestamp import Timestamp
+from cl.runtime.records.builder_util import BuilderUtil
 from cl.runtime.records.protocols import PRIMITIVE_TYPE_NAMES
 from cl.runtime.records.typename import typename
 from cl.runtime.schema.type_cache import TypeCache
 from cl.runtime.schema.type_hint import TypeHint
 
+_PASSTHROUGH_CLASS_NAMES = {"str", "float", "bool", "date","time", "datetime", "UUID", "bytes"}
+"""Class names for which the argument is returned as is after checking that a required value is not None."""
 
-class PrimitiveUtil:
-    """Helper methods for primitive types."""
+class PrimitiveUtil(BuilderUtil):
+    """Helper methods for implementing the builder pattern with primitive fields."""
 
     @classmethod
-    def normalize(cls, data: Any, type_hint: TypeHint | None = None) -> Any:
-        """Validate primitive data against the schema and return normalized value if necessary.."""
+    def build_(
+            cls,
+            data: Any,
+            type_hint: TypeHint | None = None,
+            *,
+            outer_type_name: str | None = None,
+            field_name: str | None = None,
+    ) -> Any:
 
-        # Get type name of data, which may be a type
+        # Handle None first
+        if data is None:
+            if type_hint.optional:
+                # Optional and has the value of None, return None
+                return None
+            else:
+                # Required and has the value of None, raise an error
+                location_str = cls._get_location_str(
+                    data,
+                    type_hint,
+                    outer_type_name=outer_type_name,
+                    field_name=field_name)
+                raise RuntimeError(f"Required field is not specified.{location_str}")
+
+        # Get the actual type name of data, which may be a type
         data_class_name = "type" if isinstance(data, type) else typename(data)
 
-        # Get parameters from the type chain, considering the possibility that it may be None
+        # Get the expected type name, which may include subtypes such as long or timestamp
         schema_type_name = type_hint.schema_type_name if type_hint is not None else None
-        is_optional = type_hint.optional if type_hint is not None else None
 
-        # Validate that schema_type_name is compatible with value_class_name if specified
-        # Because the value of None is passed through, value_class_name NoneType is compatible with any schema_type_name
-        if schema_type_name is not None and data_class_name != "NoneType" and schema_type_name != data_class_name:
+        # Ensure there is no remaining chain (type hint is not a sequence or mapping)
+        if type_hint.remaining:
+            raise RuntimeError(
+                f"Data is an instance of a primitive class {data_class_name} which is incompatible with type hint\n"
+                f"{type_hint.to_str()}."
+            )
+
+        # Validate that the actual data type matches the expected type (which may have subtypes)
+        if data_class_name != schema_type_name:
             if schema_type_name == "long":
                 if data_class_name != "int":
+                    location_str = cls._get_location_str(
+                        data,
+                        type_hint,
+                        outer_type_name=outer_type_name,
+                        field_name=field_name)
                     raise RuntimeError(
-                        f"Type {schema_type_name} can only be stored using int class, not {data_class_name} class."
+                        f"Type '{schema_type_name}' can only be stored as 'int' class, not {data_class_name} class."
+                        f"{location_str}"
                     )
             elif schema_type_name == "timestamp":
                 if data_class_name != "UUID":
+                    location_str = cls._get_location_str(
+                        data,
+                        type_hint,
+                        outer_type_name=outer_type_name,
+                        field_name=field_name)
                     raise RuntimeError(
-                        f"Type {schema_type_name} can only be stored using UUID class, not {data_class_name} class."
+                        f"Type '{schema_type_name}' can only be stored as 'str' class, not {data_class_name} class."
+                        f"{location_str}"
                     )
-            elif data is not None and data_class_name != schema_type_name:
-                raise RuntimeError(f"Type {schema_type_name} cannot be stored as {data_class_name} class.")
-
-        # Normalize based on value_class_name, using schema_type_name to distinguish between types that share the same class
-        if data is None:
-            if is_optional:
-                return None
             else:
+                location_str = cls._get_location_str(
+                    data,
+                    type_hint,
+                    outer_type_name=outer_type_name,
+                    field_name=field_name)
                 raise RuntimeError(
-                    f"A field of type {typename(schema_type_name)} is None but declared as required.")
-        elif data_class_name == "str":
-            return data if data else None
-        elif data_class_name == "float":
-            return data
-        elif data_class_name == "bool":
+                    f"Type {schema_type_name} cannot be stored as {data_class_name} class.{location_str}"
+                    )
+
+        # Use schema_type_name to distinguish between types that share the same class
+        if data_class_name in _PASSTHROUGH_CLASS_NAMES:
+            # We already checked that the type matches the schema and handled None
             return data
         elif data_class_name == "int":
-            if schema_type_name is not None and schema_type_name == "int":
+            if schema_type_name == "int":
                 # Check that the value is in 32-bit signed integer range
                 # if schema_type_name is specified and is int rather than long
                 check_int_32(data)
                 return data
-            elif schema_type_name is not None and schema_type_name == "long":
+            elif schema_type_name == "long":
                 # Check that the value is in 54-bit signed integer range
                 # if schema_type_name is specified and is long
                 check_int_54(data)
                 return data
             else:
-                # Otherwise do not perform range checks and use the convention for long
-                # TODO: Use conventions depending on value instead?
+                location_str = cls._get_location_str(
+                    data,
+                    type_hint,
+                    outer_type_name=outer_type_name,
+                    field_name=field_name)
+                raise RuntimeError(
+                    f"Type {schema_type_name} is stored as '{data_class_name}', use 'int' instead.{location_str}")
+        elif data_class_name == "str":
+            if schema_type_name == "str":
+                # No further checks are required
                 return data
-        elif data_class_name == "date":
-            return data
-        elif data_class_name == "time":
-            return data
-        elif data_class_name == "datetime":
-            return data
-        elif data_class_name == "UUID":
-            if data.version != 7 or (schema_type_name is not None and schema_type_name == "UUID"):
+            elif schema_type_name == "timestamp":
+                # Perform fast check for valid format
+                assert Timestamp.guard_valid(
+                    data,
+                    fast=True,
+                    value_name=field_name,
+                    data_type=outer_type_name
+                )
                 return data
             else:
-                # Under else, value.version == 7 and schema_type_name != "UUID"
-                if schema_type_name is not None and schema_type_name != "timestamp":
-                    raise RuntimeError("For UUID version 7, only UUID or timestamp types are valid.")
-                return data
-        elif data_class_name == "bytes":
-            return data
+                location_str = cls._get_location_str(
+                    data,
+                    type_hint,
+                    outer_type_name=outer_type_name,
+                    field_name=field_name)
+                raise RuntimeError(
+                    f"Type {schema_type_name} is stored as '{data_class_name}', use 'int' instead.{location_str}")
         elif data_class_name == "type":
             assert TypeCache.guard_known_type(data)
             return data
