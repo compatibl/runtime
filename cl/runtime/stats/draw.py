9cecf519b498dfde7187cdb7ef61638550fead22
Alexander Sokol <sokol@compatibl.com>
2025-08-24 13:53:01 -0400

    Rename LlmDraw to Draw and move to runtime package.
    (cherry picked from commit 4002fb57d8abd36ef6c6f9ef9d6bd1704a809e80)

---
+++ cl/runtime/stats/draw.py
@@ -0,0 +1,40 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from dataclasses import dataclass
+from cl.runtime.contexts.context_manager import active_or_none
+from cl.runtime.records.for_dataclasses.extensions import required
+from cl.runtime.records.record_mixin import RecordMixin
+from cl.runtime.stats.draw_key import DrawKey
+
+
+@dataclass(slots=True, kw_only=True)
+class Draw(DrawKey, RecordMixin):
+    """Keep track of a unique draw identifier consisting of dot-delimited draw indices for each nested draw context."""
+
+    draw_index: int = required()
+    """Zero-based draw index for this trial is the last token of draw_id."""
+
+    def get_key(self) -> DrawKey:
+        return DrawKey(draw_id=self.draw_id).build()
+
+    def __init(self) -> None:
+        """Use instead of __init__ in the builder pattern, invoked by the build method in base to derived order."""
+        # Unique draw identifier consists of dot-delimited draw indices for each nested draw context
+        if current_draw := active_or_none(type(self)):
+            # Combine the active draw_id with self.draw_index
+            self.draw_id = f"{current_draw.draw_id}.{self.draw_index}"
+        else:
+            # No active draw_id, convert to string
+            self.draw_id = str(self.draw_index)
---
+++ tests/cl/runtime/stats/test_draw.py
@@ -0,0 +1,59 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+from cl.runtime.contexts.context_manager import activate
+from cl.runtime.contexts.context_manager import active
+from cl.runtime.contexts.context_manager import active_or_none
+from cl.runtime.stats.draw import Draw
+
+
+def test_context():
+    """Test Draw context."""
+
+    assert active_or_none(Draw) is None
+    with activate(Draw(draw_index=1).build()) as draw_1:
+        # One token in chain
+        assert draw_1.draw_id == "1"
+        assert active(Draw).draw_id == "1"
+        with activate(Draw(draw_index=2).build()) as draw_12:
+            # Two tokens in chain
+            assert draw_12.draw_id == "1.2"
+            assert active(Draw).draw_id == "1.2"
+        assert active(Draw).draw_id == "1"
+        with activate(Draw(draw_index=3).build()) as draw_13:
+            # Two tokens in chain
+            assert draw_13.draw_id == "1.3"
+            assert active(Draw).draw_id == "1.3"
+    assert active_or_none(Draw) is None
+
+
+def test_exceptions():
+    """Test Draw exceptions."""
+
+    not_a_str_msg = "not a valid type for a field declared as int"
+    required_msg = "Required field is not specified"
+    with pytest.raises(RuntimeError, match=not_a_str_msg):
+        # Not an int
+        Draw(draw_index="1").build()  # noqa
+    with pytest.raises(RuntimeError, match=not_a_str_msg):
+        # Empty string
+        Draw(draw_index="").build()  # noqa
+    with pytest.raises(RuntimeError, match=required_msg):
+        # None
+        Draw(draw_index=None).build()  # noqa
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
