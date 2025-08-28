1703e6ea6251560cada8b3561158e137ee220c20
Alexander Sokol <sokol@compatibl.com>
2025-08-28 11:22:48 -0400

    Add a helper class for identifier validation.
    (cherry picked from commit 8501bc9d580ba45a5582c7e6e444206663e1c651)

---
+++ cl/runtime/primitive/identifier_util.py
@@ -0,0 +1,88 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Any, TypeGuard, Sequence, Collection
+import unicodedata
+from cl.runtime.records.typename import typename
+
+_UNICODE_CATEGORY_PREFIXES = {"L", "N"}
+"""Allowed Unicode category prefixes."""
+
+_DEFAULT_IDENTIFIER_DELIMITERS = frozenset({" ", "-", "_", "."})
+"""Default identifier delimiters when not specified."""
+
+_DIRECTORY_SEPARATORS = frozenset(list(_DEFAULT_IDENTIFIER_DELIMITERS) + [":", "\\", "/"])
+"""Directory separators."""
+
+_BRACES = frozenset(list(_DEFAULT_IDENTIFIER_DELIMITERS) + ["{", "}"])
+"""Braces for formatted strings."""
+
+class IdentifierUtil:
+    """Utilities for working with Unicode identifiers."""
+
+    @classmethod
+    def guard_valid_identifier(
+            cls,
+            value: Any,
+            *,
+            identifier_delimiters: Collection[str] = _DEFAULT_IDENTIFIER_DELIMITERS,
+            allow_directory_separators: bool = False,
+            allow_braces: bool = False,
+            raise_on_fail: bool = True,
+    ) -> TypeGuard[str]:
+        """
+        Returns true if the string contains only Unicode characters, numbers, or the specified delimiters.
+
+        Args:
+            value: The identifier to validate
+            identifier_delimiters: The list of allowed delimiters
+            allow_directory_separators: Add directory separators to allowed delimiters
+            allow_braces: Add braces {} to allowed delimiters
+            raise_on_fail: If the validation fails, return None or raise an error depending on raise_on_fail
+        """
+        if not isinstance(value, str):
+            if raise_on_fail:
+                raise RuntimeError(f"Identifier '{typename(value)}' type is not string.")
+            else:
+                return False
+
+        # Expand the set of delimiters based on the specified options
+        delimiters = list(identifier_delimiters)
+        if allow_directory_separators:
+            delimiters.extend(_DIRECTORY_SEPARATORS)
+        if allow_braces:
+            delimiters.extend(_BRACES)
+        delimiters = frozenset(delimiters)
+
+        for pos, char in enumerate(value):
+            # Get two-letter Unicode category, e.g., Lu = uppercase letter or Nd = decimal digit,
+            # and check that the first letter matches L = Letter or N = Number
+            if (category := unicodedata.category(char))[0] in _UNICODE_CATEGORY_PREFIXES:
+                # Continue if allowed category
+                continue
+            elif char in delimiters:
+                # Continue if allowed delimiter
+                continue
+            elif raise_on_fail:
+                allowed_categories_str = ", ".join(_UNICODE_CATEGORY_PREFIXES)
+                allowed_delimiters_str = ", ".join(delimiters)
+                raise RuntimeError(
+                    f"Character '{char}' at position {pos} in identifier '{value}' has Unicode category {category}\n"
+                    f"which does not have one of the allowed Unicode category prefixes: {allowed_categories_str}\n"
+                    f"and it is also not not one of the allowed delimiters: {allowed_delimiters_str}")
+            else:
+                return False
+
+        # Passed validation if the loop completed
+        return True
---
+++ tests/cl/runtime/primitive/test_identifier_util.py
@@ -0,0 +1,98 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+from cl.runtime.primitive.identifier_util import IdentifierUtil
+
+
+def test_guard_valid_identifier():
+    """Test for 'guard_valid_identifier' method."""
+
+    # Valid cases
+    IdentifierUtil.guard_valid_identifier("abc")  # Allow identifier without delimiter
+    IdentifierUtil.guard_valid_identifier("abc.xyz")  # Allow identifier with delimiter
+
+    # Invalid cases
+    with pytest.raises(RuntimeError):
+        IdentifierUtil.guard_valid_identifier("abc|xyz")
+    with pytest.raises(RuntimeError):
+        IdentifierUtil.guard_valid_identifier("abc(xyz")
+    with pytest.raises(RuntimeError):
+        IdentifierUtil.guard_valid_identifier("abc)xyz")
+    with pytest.raises(RuntimeError):
+        IdentifierUtil.guard_valid_identifier("abc\\xyz")
+    with pytest.raises(RuntimeError):
+        IdentifierUtil.guard_valid_identifier("abc/xyz")
+    with pytest.raises(RuntimeError):
+        IdentifierUtil.guard_valid_identifier("abc{xyz")
+    with pytest.raises(RuntimeError):
+        IdentifierUtil.guard_valid_identifier("abc}xyz")
+
+    # Test with raise_on_fail = False
+    assert not IdentifierUtil.guard_valid_identifier("abc|xyz", raise_on_fail=False)
+
+
+def test_guard_valid_directory():
+    """Test for 'check_valid_path' method."""
+
+    # Valid cases
+    IdentifierUtil.guard_valid_identifier("abc", allow_directory_separators=True)  # Allow path without delimiter
+    IdentifierUtil.guard_valid_identifier("abc.xyz", allow_directory_separators=True)  # Allow path with delimiter
+    IdentifierUtil.guard_valid_identifier("mydir\\mydir\\abc.xyz", allow_directory_separators=True)
+    IdentifierUtil.guard_valid_identifier("mydir/mydir/abc.xyz", allow_directory_separators=True)
+
+    # Invalid cases
+    with pytest.raises(RuntimeError):
+        IdentifierUtil.guard_valid_identifier("abc(xyz", allow_directory_separators=True)
+    with pytest.raises(RuntimeError):
+        IdentifierUtil.guard_valid_identifier("abc)xyz", allow_directory_separators=True)
+    with pytest.raises(RuntimeError):
+        IdentifierUtil.guard_valid_identifier("abc|xyz", allow_directory_separators=True)
+    with pytest.raises(RuntimeError):
+        IdentifierUtil.guard_valid_identifier("mydir\\mydir\\abc[xyz", allow_directory_separators=True)
+    with pytest.raises(RuntimeError):
+        IdentifierUtil.guard_valid_identifier("abc{xyz", allow_directory_separators=True)
+    with pytest.raises(RuntimeError):
+        IdentifierUtil.guard_valid_identifier("abc}xyz", allow_directory_separators=True)
+
+    # Test with raise_on_fail = False
+    assert not IdentifierUtil.guard_valid_identifier("abc|xyz", allow_directory_separators=True, raise_on_fail=False)
+
+def test_guard_valid_format():
+    """Test for 'guard_valid_identifier' method."""
+
+    # Valid cases
+    IdentifierUtil.guard_valid_identifier("abc", allow_braces=True)  # Allow identifier without delimiter
+    IdentifierUtil.guard_valid_identifier("abc.xyz", allow_braces=True)  # Allow identifier with delimiter
+    IdentifierUtil.guard_valid_identifier("abc{xyz", allow_braces=True)
+    IdentifierUtil.guard_valid_identifier("abc}xyz", allow_braces=True)
+
+    # Invalid cases
+    with pytest.raises(RuntimeError):
+        IdentifierUtil.guard_valid_identifier("abc|xyz", allow_braces=True)
+    with pytest.raises(RuntimeError):
+        IdentifierUtil.guard_valid_identifier("abc(xyz", allow_braces=True)
+    with pytest.raises(RuntimeError):
+        IdentifierUtil.guard_valid_identifier("abc)xyz", allow_braces=True)
+    with pytest.raises(RuntimeError):
+        IdentifierUtil.guard_valid_identifier("abc\\xyz", allow_braces=True)
+    with pytest.raises(RuntimeError):
+        IdentifierUtil.guard_valid_identifier("abc/xyz", allow_braces=True)
+
+    # Test with raise_on_fail = False
+    assert not IdentifierUtil.guard_valid_identifier("abc|xyz", allow_braces=True, raise_on_fail=False)
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
