1cde02180974b443004390edc1ef081cb28b12db
Alexander Sokol <sokol@compatibl.com>
2025-09-01 01:18:43 -0400

    Simplify TypeSchema.
    (cherry picked from commit 5be7795833a169ad61116af51582d16c55c39b76)

--- cl/runtime/schema/type_schema.py
+++ cl/runtime/schema/type_schema.py
@@ -18,6 +18,9 @@ from enum import Enum
 from types import ModuleType
 from typing import Mapping
 from uuid import UUID
+
+from memoization import cached
+
 from cl.runtime.records.conditions import Condition
 from cl.runtime.records.protocols import is_data_key_or_record_type
 from cl.runtime.records.typename import typename
@@ -36,72 +39,40 @@ class TypeSchema:
     enums and primitive types.
     """
 
-    _spec_dict: dict[str, TypeSpec] = {
-        "str": PrimitiveSpec.from_class(str),
-        "timestamp": PrimitiveSpec.from_class(str, subtype="timestamp"),  # TODO: Do we need these cases when using subtype?
-        "float": PrimitiveSpec.from_class(float),
-        "bool": PrimitiveSpec.from_class(bool),
-        "int": PrimitiveSpec.from_class(int),
-        "long": PrimitiveSpec.from_class(int, subtype="long"),  # TODO: Do we need these cases when using subtype?
-        "date": PrimitiveSpec.from_class(dt.date),
-        "time": PrimitiveSpec.from_class(dt.time),
-        "datetime": PrimitiveSpec.from_class(dt.datetime),
-        "UUID": PrimitiveSpec.from_class(UUID),
-        "bytes": PrimitiveSpec.from_class(float),
-    }
-    """Dictionary of type specs indexed by type name and initialized with primitive types."""
-
-    _class_dict: Mapping[str, type] | None = None
-    """Dictionary of types indexed by class name."""
-
-    _modules: tuple[ModuleType, ...] | None = None
-    """Modules from the packages specified in the settings."""
-
-    _packages: tuple[str, ...] | None = None
-    """Packages specified in the settings."""
-
     @classmethod
+    @cached
     def for_type_name(cls, type_name: str) -> TypeSpec:
         """Get or create type spec for the specified type name."""
-        if (result := cls._spec_dict.get(type_name, None)) is not None:
-            # Already created, return from spec dictionary
-            return result
-        else:
-            # Get class for the specified type name and use it to get type spec
-            class_ = TypeCache.from_type_name(type_name)
-            return cls.for_class(class_)
+        # Get class for the specified type name and use it to get type spec
+        class_ = TypeCache.from_type_name(type_name)
+        return cls.for_class(class_)
 
     @classmethod
+    @cached
     def for_class(cls, class_: type) -> TypeSpec:
         """Get or create type spec for the specified class."""
         type_name = typename(class_)
-        if (result := cls._spec_dict.get(type_name, None)) is not None:
-            # Already created, return from spec dictionary
-            return result
+        # TODO: ! Use mapping records to avoid hardcoding the list of data frameworks
+        # Get class for the type spec
+        if issubclass(class_, Enum):
+            # Enum class
+            spec_class = EnumSpec
+        elif dataclasses.is_dataclass(class_):
+            # Uses dataclasses
+            spec_class = DataclassSpec
+        elif is_data_key_or_record_type(class_) and not class_.get_field_names():  # noqa
+            # Base class of data, key or record with no slots
+            spec_class = NoSlotsSpec
+        elif issubclass(class_, Condition):
+            # Condition class
+            spec_class = ConditionSpec
         else:
+            raise RuntimeError(
+                f"Class {typename(class_)} implements build method but does not\n"
+                f"use one of the supported dataclass frameworks and does not\n"
+                f"have a method to generate type spec."
+            )
 
-            # TODO: Use mapping records to avoid hardcoding the list of data frameworks
-            # Get class for the type spec
-            if issubclass(class_, Enum):
-                # Enum class
-                spec_class = EnumSpec
-            elif dataclasses.is_dataclass(class_):
-                # Uses dataclasses
-                spec_class = DataclassSpec
-            elif is_data_key_or_record_type(class_) and not class_.get_field_names():  # noqa
-                # Base class of data, key or record with no slots
-                spec_class = NoSlotsSpec
-            elif issubclass(class_, Condition):
-                # Condition class
-                spec_class = ConditionSpec
-            else:
-                raise RuntimeError(
-                    f"Class {typename(class_)} implements build method but does not\n"
-                    f"use one of the supported dataclass frameworks and does not\n"
-                    f"have a method to generate type spec."
-                )
-
-            # Create from class, add to spec dictionary and return
-            result = spec_class.from_class(class_)
-            cls._spec_dict[type_name] = result
-            return result
+        # Create from class, add to spec dictionary and return
+        result = spec_class.from_class(class_)
+        return result
