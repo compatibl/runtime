4d9a1e17ae3d66a9d53fba2ec1d41ed75ad65e57
Roman Kushnir <romank@compatibl.com>
2025-09-03 14:45:02 +0200

    Add test for get_common_base_record_type_name() method.
    (cherry picked from commit c5b7cefd6a00716c52bd41e87d086468049a2a11)
    (cherry picked from commit 85214768b14325857b6968efe792ac346fbfec51)

---
+++ tests/cl/runtime/db/test_bindings.py
@@ -0,0 +1,43 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from cl.runtime.contexts.context_manager import active
+from cl.runtime.db.data_source import DataSource
+from stubs.cl.runtime import StubDataclassDerived, StubDataclassKey, StubDataclass
+from stubs.cl.runtime import StubDataclassDoubleDerived
+from stubs.cl.runtime import StubDataclassOtherDerived
+
+
+def test_get_common_base(default_db_fixture):
+    """Test get common base type function."""
+    
+    ds: DataSource = active(DataSource)
+
+    records = [
+        StubDataclassDerived(id="stub_derived").build(),
+        StubDataclassDoubleDerived(id="stub_double_derived").build(),
+    ]
+
+    # Save derived records
+    ds.replace_many(records, commit=True)
+
+    # Expect the base type to be 'StubDataclassDerived'
+    common_base_type = ds.get_common_base_record_type(key_type=StubDataclassKey)
+    assert common_base_type == StubDataclassDerived
+
+    # Save other derived record, then the base type should be 'StubDataclass'
+    ds.replace_one(StubDataclassOtherDerived(id="stub_other_derived").build(), commit=True)
+
+    common_base_type = ds.get_common_base_record_type(key_type=StubDataclassKey)
+    assert common_base_type == StubDataclass
