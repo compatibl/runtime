8cf4c7bd3f968b43dfa6ebea795ff3eedbf97fdc
Alexander Sokol <sokol@compatibl.com>
2025-09-21 15:47:11 -0400

    Add a second set of points forming a surface to the scatter plot 3D test.
    (cherry picked from commit 6b6beb5b1c71fa2d66342ac574c66a78f89a2ec0)

--- tests/cl/runtime/plots/test_scatter_plot_3d.py
+++ tests/cl/runtime/plots/test_scatter_plot_3d.py
@@ -1,13 +1,32 @@
 import pytest
+import numpy as np
 
 from cl.runtime.plots.for_matplotlib.plotly_engine import PlotlyEngine
+from cl.runtime.plots.plot_surface_style import PlotSurfaceStyle
 from cl.runtime.plots.scatter_plot_3d import ScatterPlot3D
 from cl.runtime.plots.scatter_values_3d import ScatterValues3D
 
 def create_plot():
     """Create plot for testing."""
+    # Create a grid for the surface
+    x = np.linspace(0, 4, 20)
+    y = np.linspace(0, 8, 20)
+    x_grid, y_grid = np.meshgrid(x, y)
+    z_grid = 0.5 * x_grid + 0.3 * y_grid**2  # Simple surface function
+
     data = [
-        ScatterValues3D(x=[1, 2, 3], y=[4, 5, 6], z=[7, 8, 9])
+        ScatterValues3D(
+            x=[1, 2, 3],
+            y=[4, 5, 6],
+            z=[7, 8, 9],
+        ),
+        ScatterValues3D(
+            x=list(x_grid.flatten()),
+            y=list(y_grid.flatten()),
+            z=list(z_grid.flatten()),
+            marker_style=None,
+            surface_style=PlotSurfaceStyle.SOLID,
+        )
     ]
     plot = ScatterPlot3D(
         data=data,
