024f632dbd891881de41c5bf9237396505393d33
Alexander Sokol <sokol@compatibl.com>
2025-10-08 00:30:38 -0400

    Made plot engine universal for regular and irregular grids.
    (cherry picked from commit a7852f23ef45eda449381216381e12eca39e81e6)

--- cl/runtime/plots/for_matplotlib/plotly_engine.py
+++ cl/runtime/plots/for_matplotlib/plotly_engine.py
@@ -17,10 +17,8 @@ import plotly.graph_objects as go
 import plotly.io as pio
 from cl.runtime.plots.plot import Plot
 from cl.runtime.plots.plot_color import PlotColor
-from cl.runtime.plots.plot_line_style import PlotLineStyle
 from cl.runtime.plots.plot_surface_style import PlotSurfaceStyle
 from cl.runtime.plots.plotting_engine import PlottingEngine
-from cl.runtime.plots.scatter_plot_2d import ScatterPlot2D
 from cl.runtime.plots.scatter_plot_3d import ScatterPlot3D
 from cl.runtime.records.typename import typenameof
 
@@ -37,6 +35,7 @@ class PlotlyEngine(PlottingEngine):
             fig = go.Figure()
             for values in plot.data:
                 color_css = self._to_css_color(values.color)
+                colorscale = [[0.0, color_css], [1.0, color_css]]
                 marker_dict = {}
                 if values.marker_style is not None:
                     # TODO: !!!! Map marker_style to Plotly marker attributes
@@ -44,42 +43,20 @@ class PlotlyEngine(PlottingEngine):
 
                 # If a solid surface is requested, render as a continuous surface
                 if values.surface_style  == PlotSurfaceStyle.SOLID:
-                    x_list = list(values.x)
-                    y_list = list(values.y)
-                    z_list = list(values.z)
-                    # Infer a rectangular grid from unique x and y values
-                    unique_x = sorted(set(x_list))
-                    unique_y = sorted(set(y_list))
-                    if len(unique_x) * len(unique_y) == len(x_list):
-                        # Build a z-matrix indexed as z[y_index][x_index]
-                        z_map = {(x, y): z for x, y, z in zip(x_list, y_list, z_list)}
-                        z_matrix = [[z_map[(x, y)] for x in unique_x] for y in unique_y]
-                        colorscale = [[0.0, color_css], [1.0, color_css]]
-                        fig.add_trace(
-                            go.Surface(
-                                x=unique_x,
-                                y=unique_y,
-                                z=z_matrix,
-                                colorscale=colorscale,
-                                showscale=False,
-                                opacity=0.8,
-                                name=values.legend,
-                                # Surface traces can be part of legend when named; ensure legend is shown
-                                showlegend=True,
-                            )
+
+                    # Fallback to markers if data cannot form a grid
+                    fig.add_trace(
+                        go.Mesh3d(
+                            x=values.x,
+                            y=values.y,
+                            z=values.z,
+                            colorscale=colorscale,
+                            showscale=False,
+                            opacity=0.8,
+                            showlegend=True,
+                            name=values.legend,
                         )
-                    else:
-                        # Fallback to markers if data cannot form a grid
-                        fig.add_trace(
-                            go.Scatter3d(
-                                x=values.x,
-                                y=values.y,
-                                z=values.z,
-                                mode="markers",
-                                marker={"color": color_css, "size": 2, **marker_dict},
-                                name=values.legend,
-                            )
-                        )
+                    )
                 else:
                     # Default: render as markers
                     fig.add_trace(
