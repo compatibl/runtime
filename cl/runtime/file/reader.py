52c15f37cf2b30f15e10dce5641b5f0cf0fa6e3a
Alexander Sokol <sokol@compatibl.com>
2025-12-01 19:55:33 -0500

    Restore Reader class and rename CsvFileUtil to CsvReader.
    (cherry picked from commit fca5ccbabf10160e9d7c72e8c8bcbab2c41085b4)

---
+++ cl/runtime/file/reader.py
@@ -0,0 +1,29 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from abc import ABC
+from abc import abstractmethod
+from dataclasses import dataclass
+from cl.runtime.file.reader_key import ReaderKey
+from cl.runtime.records.record_mixin import RecordMixin
+
+
+@dataclass(slots=True, kw_only=True)
+class Reader(ReaderKey, RecordMixin, ABC):
+    """Read records from the specified storage and save them to the active data source."""
+
+    def get_key(self) -> ReaderKey:
+        return ReaderKey(reader_id=self.reader_id).build().build()
+
+
