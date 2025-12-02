7f49578a3ffef43cf0f8da39d5add4cd81afa6e0
Alexander Sokol <sokol@compatibl.com>
2025-12-01 21:00:56 -0500

    Remove the obsolete script for fixing timestamps.
    (cherry picked from commit 4da0643af30f3c08ff6036827db0019b1bb53603)

--- cl/runtime/prebuild/timestamp_format_util.py
+++ cl/runtime/prebuild/timestamp_format_util.py
@@ -15,6 +15,8 @@
 import os
 import re
 from fnmatch import fnmatch
+from typing import Sequence
+
 from cl.runtime.settings.env_settings import EnvSettings
 from cl.runtime.settings.project_settings import ProjectSettings
 
@@ -46,57 +48,3 @@ class TimestampFormatUtil:
         # Write back to the file
         with open(file_path, "w", encoding="utf-8") as file:
             file.write(content)
-
-    @classmethod
-    def update_timestamps(
-        cls,
-        *,
-        file_include_patterns: list[str] | None = None,
-        file_exclude_patterns: list[str] | None = None,
-    ) -> None:
-        """
-        Check that timestamp format is correct inside the specified files under the project root.
-
-        Args:
-            file_include_patterns: Optional list of filename glob patterns to include
-            file_exclude_patterns: Optional list of filename glob patterns to exclude
-        """
-
-        # The list of packages from context settings
-        packages = EnvSettings.instance().env_packages
-
-        missing_files = []
-        all_root_paths = set()
-        for package in packages:
-            # Add paths to source and stubs directories
-            if (x := ProjectSettings.get_source_root(package)) is not None and x not in all_root_paths:
-                all_root_paths.add(x)
-            if (x := ProjectSettings.get_stubs_root(package)) is not None and x not in all_root_paths:
-                all_root_paths.add(x)
-            if (x := ProjectSettings.get_tests_root(package)) is not None and x not in all_root_paths:
-                all_root_paths.add(x)
-            if (x := ProjectSettings.get_preloads_root(package)) is not None and x not in all_root_paths:
-                all_root_paths.add(x)
-
-        # Use default include patterns if not specified by the caller
-        if file_include_patterns is None:
-            file_include_patterns = ["*.csv", "*.txt"]
-
-        # Use default exclude patterns if not specified by the caller
-        if file_exclude_patterns is None:
-            file_exclude_patterns = []
-
-        # Apply to each element of root_paths
-        files_with_error = []
-        for root_path in all_root_paths:
-            # Walk the directory tree
-            for dir_path, dir_names, filenames in os.walk(root_path):
-                # Apply exclude patterns
-                filenames = [x for x in filenames if not any(fnmatch(x, y) for y in file_exclude_patterns)]
-                # Apply include patterns
-                filenames = [x for x in filenames if any(fnmatch(x, y) for y in file_include_patterns)]
-                # Iterate over filenames
-                for filename in filenames:
-                    # Load the file
-                    file_path = str(os.path.join(dir_path, filename))
-                    TimestampFormatUtil.update_file(file_path)
