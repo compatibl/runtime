9ab5501e1cc246c39ce5dfd3731434e0fb6350be
Alexander Sokol <sokol@compatibl.com>
2025-12-10 19:04:45 -0500

    Improve error message.
    (cherry picked from commit c1f20545f00eb24a3ea5a4f5fffa504e32682814)

--- cl/runtime/file/csv_reader.py
+++ cl/runtime/file/csv_reader.py
@@ -139,7 +139,7 @@ class CsvReader(Reader):
             files_list = "".join([f"    {file}\n" for file in files_with_error])
             msg = (
                 f"Found values that should be wrapped in quotes to stop Excel from modifying them on save.\n"
-                f"Run fix_csv_quotes script to fix. CSV preload file(s) that have this error:\n{files_list}"
+                f"RECOMMENDED ACTION: Run fix_csv_quotes script to fix.\n{files_list}"
             )
             if not apply_fix:
                 raise RuntimeError(msg)
